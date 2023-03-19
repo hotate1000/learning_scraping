5286d964bead531a4cef8e0facfb7edda6377a14
hotate1000 <ooo.336.sss@gmail.com>
2023-03-20 00:25:35 +0900

    初回コミット

---
+++ chap1/chap1-1.py
@@ -0,0 +1,12 @@
+import requests
+
+from pathlib import Path
+import sys
+sys.path.append(str(Path(__file__).resolve().parent.parent))
+import sample_data
+
+url = sample_data.sample_url;
+response = requests.get(url);
+print(response);
+response.encoding = response.apparent_encoding;
+print(response.text);
